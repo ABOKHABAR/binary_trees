c861f6e3dce19e923bc888580f02d6a82842a786
ABOKHABAR <amossab89@gmail.com>
2024-02-29 05:29:57 -0800

    khabo

--- binary_trees.h
+++ binary_trees.h
@@ -18,14 +18,14 @@
 struct binary_tree_s
 {
 	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+	struct unique_binary_tree_s *parent;
+	struct unique_binary_tree_s *left;
+	struct unique_binary_tree_s *right;
 };
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+typedef struct unique_binary_tree_s binary_tree_t;
+typedef struct unique_binary_tree_s bst_t;
+typedef struct unique_binary_tree_s avl_t;
+typedef struct unique_binary_tree_s heap_t;
 /*  linked list for advanced 101 the levelorder */
 /**
  * struct link_s - structure for advanced tasks
@@ -37,30 +37,30 @@ typedef struct binary_tree_s heap_t;
 typedef struct link_s
 {
 	size_t n;
-	struct binary_tree_s const *node;
+	struct unique_binary_tree_s const *node;
 	struct link_s *next;
 } link_t;
 /*  ----------------- mandatory task ---------------------------------*/
-void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
-size_t binary_tree_size(const binary_tree_t *tree);
-size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_nodes(const binary_tree_t *tree);
-int binary_tree_balance(const binary_tree_t *tree);
-int binary_tree_is_full(const binary_tree_t *tree);
-int binary_tree_is_perfect(const binary_tree_t *tree);
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void unique_binary_tree_print(const unique_binary_tree_t *);
+unique_binary_tree_t *unique_binary_tree_node(unique_binary_tree_t *parent, int value);
+unique_binary_tree_t *unique_binary_tree_insert_left(unique_binary_tree_t *parent, int value);
+unique_binary_tree_t *unique_binary_tree_insert_right(unique_binary_tree_t *parent, int value);
+void unique_binary_tree_delete(unique_binary_tree_t *tree);
+int unique_binary_tree_is_leaf(const unique_binary_tree_t *node);
+int unique_binary_tree_is_root(const unique_binary_tree_t *node);
+void unique_binary_tree_preorder(const unique_binary_tree_t *tree, void (*func)(int));
+void unique_binary_tree_inorder(const unique_binary_tree_t *tree, void (*func)(int));
+void unique_binary_tree_postorder(const unique_binary_tree_t *tree, void (*func)(int));
+size_t unique_binary_tree_height(const unique_binary_tree_t *tree);
+size_t unique_binary_tree_depth(const unique_binary_tree_t *tree);
+size_t unique_binary_tree_size(const unique_binary_tree_t *tree);
+size_t unique_binary_tree_leaves(const unique_binary_tree_t *tree);
+size_t unque_binary_tree_nodes(const unique_binary_tree_t *tree);
+int unique_binary_tree_balance(const unique_binary_tree_t *tree);
+int unique_binary_tree_is_full(const unique_binary_tree_t *tree);
+int unique_binary_tree_is_perfect(const unique_binary_tree_t *tree);
+unique_binary_tree_t *unique_binary_tree_sibling(unique_binary_tree_t *node);
+unique_binary_tree_t *unique_binary_tree_uncle(unique_binary_tree_t *node);
 /*  ------------------ advanced task -----------------------------------*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
